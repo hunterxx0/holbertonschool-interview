1434b3244206089a015f1ce3c4575b8b4a1182bb
hunterxx0 <azouzmhamed@gmail.com>
2021-01-07 18:02:09 +0100

    first

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,8 +3,9 @@
 
 def minOperations(n):
     op = 0
-    for i in range(2, n):
-        while(n % i == 0):
-            op += i
-            n /= i
+    if n >= 2:
+        for i in range(2, n):
+            while(n % i == 0):
+                op += i
+                n /= i
     return(op)
