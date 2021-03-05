8abd268ba6133e3fa6b27067df5e9caf5592502a
hunterxx0 <azouzmhamed@gmail.com>
2021-03-05 17:02:30 +0100

    first

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,5 +1,26 @@
 #include "lists.h"
 /**
+ * checkPalin - checks if a linked list is palindrome
+ *
+ * @left: left node
+ * @right: right node
+ * Return: 0 or 1
+ */
+int checkPalin(listint_t** left, listint_t* right)
+{
+    // base case
+    if (right == NULL) {
+        return 1;
+    }
+ 
+    int result = checkPalin(left, right->next) &&
+            ((*left)->n == right->n);
+    (*left) = (*left)->next;
+ 
+    return result;
+}
+
+/**
  * is_palindrome - checks if a linked list is palindrome
  *
  * @head: Head node
@@ -7,54 +28,5 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = *head, *tmp = NULL, *last = NULL;
-	int c = 0, half = 0, count = 0;
-
-	if (*head)
-	{
-		while (h)
-		{
-			c += 1;
-			if (!h->next)
-				last = h;
-			h = h->next;
-		}
-		count = c;
-		half = c/2;
-		c = 0;
-		h = *head;
-		if (h->n != last->n)		
-			return (0);
-		while (c <= half)
-		{
-			if (!tmp && c == half - 1 && (count % 2))
-				tmp = h->next;
-			if (!tmp && c == half - 1 && !(count % 2))
-				tmp = h;
-			c += 1;
-			h = h->next;
-		}
-		c = 0;
-		h = *head;
-		while (half)
-		{ 
-			c += 1;
-			h = h->next; 
-			if (c == half)
-			{
-				if (h->n != tmp->n)
-					return (0);
-				else
-				{
-					half -= 1;
-					h = *head;
-					c = 0;
-					tmp = tmp->next;
-				}
-			}	
-			if (!tmp)
-				break;
-			}
-		}
-	return (1);
+	return checkPalin(head, *head);
 }
