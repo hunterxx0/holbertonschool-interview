44851af1498c0ccaea7745b3ed41179fc318b042
hunterxx0 <azouzmhamed@gmail.com>
2021-03-06 15:28:27 +0100

    first

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import signal
+import sys
+
+c = 0
+size = 0
+codes = {'200': 0,
+         '301': 0,
+         '400': 0,
+         '401': 0,
+         '403': 0,
+         '404': 0,
+         '405': 0,
+         '500': 0
+         }
+
+def signal_handler(sig, frame):
+	try:
+		print("File size: {}".format(size))
+		for x, y in codes.items():
+			if y:
+				print('{}: {}'.format(x, y))
+	except KeyboardInterrupt:
+		raise KeyboardInterrupt
+
+signal.signal(signal.SIGINT, signal_handler)
+
+for line in sys.stdin:
+	num = line.split('"')[-1].split(" ")[1:]
+	if len(num) != 2:
+		continue
+	c +=1
+	if num[0] in codes.keys():
+		codes[num[0]] += 1
+	size += int(num[1])
+	if c % 10 == 0:
+		print("File size: {}".format(size))
+		for x, y in codes.items():
+			if y:
+				print('{}: {}'.format(x, y))
