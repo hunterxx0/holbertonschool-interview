c3a16c26c512ccb85cc752a8b4bfd3b0d3878f8e
hunterxx0 <azouzmhamed@gmail.com>
2021-09-23 11:33:23 +0100

    first

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif /* _HOLBERTON_H_ */
