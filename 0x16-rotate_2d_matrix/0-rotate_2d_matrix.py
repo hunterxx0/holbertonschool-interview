9bfaec40e42213f82712633841c613cb7a9c9e31
hunterxx0 <azouzmhamed@gmail.com>
2021-09-29 14:49:34 +0100

    first

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
 
 def rotate_2d_matrix(matrix):
+    """
+    Rotate 2D Matrix
+    """
     size = len(matrix)
     arr = []
     for x in range(0, size):
-    	arr.append([i[x] for i in matrix][::-1])
+        arr.append([i[x] for i in matrix][::-1])
     for x in range(0, size):
-       matrix[x] = arr[x]
-       
+        matrix[x] = arr[x]
