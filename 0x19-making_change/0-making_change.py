e702dd9e1d2b1c4228f24d3cd70bd2cb3a690d06
hunterxx0 <azouzmhamed@gmail.com>
2021-10-26 23:56:33 +0100

    first

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+makeChange Method
+"""
+
+def makeChange(coins, total):
+	"""
+	Given a pile of coins of different values,
+	determine the fewest number of coins needed
+	to meet a given amount total.
+	"""
+	if total <= 0:
+		return 0
+	value = 0
+	while(len(coins) and total):
+		mx = max(coins)
+		result = total // mx
+		rest = mx % total
+		coins.remove(mx)
+		if not result:
+			continue
+		total -= mx * result
+		value += result
+	if not total:
+		return value
+	return -1
