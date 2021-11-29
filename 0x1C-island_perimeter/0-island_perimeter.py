477373c172d0c44a5a09bf92c83020e49b3e5ce9
hunterxx0 <azouzmhamed@gmail.com>
2021-11-29 08:42:00 +0100

    first

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,23 +5,23 @@ makeChange Method
 
 
 def count(g, x, y):
-  c = 0
-  ll = [1, -2, 1]
-  if y+1 != len(g[x]) and g[x][y+1] == 0:
-    c += 1
-  if y > 0 and g[x][y-1] == 0:
-    c += 1
-  if x+1 != len(g) and g[x+1][y] == 0:
-    c += 1
-  if x > 0 and g[x-1][y] == 0:
-    c += 1
-  return c
+    c = 0
+    ll = [1, -2, 1]
+    if y+1 != len(g[x]) and g[x][y+1] == 0:
+        c += 1
+    if y > 0 and g[x][y-1] == 0:
+        c += 1
+    if x+1 != len(g) and g[x+1][y] == 0:
+        c += 1
+    if x > 0 and g[x-1][y] == 0:
+        c += 1
+    return c
+
 
 def island_perimeter(grid):
-  c = 0
-  for x in range(0, len(grid)):
-    for y in range(0, len(grid[x])):
-      if grid[x][y] == 1:
-        c += count(grid, x, y)
-  return c
-  
+    c = 0
+    for x in range(0, len(grid)):
+        for y in range(0, len(grid[x])):
+            if grid[x][y] == 1:
+                c += count(grid, x, y)
+    return c
