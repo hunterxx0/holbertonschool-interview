c501c56b72a55af6b426bdbdb7de3fd679c8a1a9
hunterxx0 <azouzmhamed@gmail.com>
2021-12-09 07:51:01 +0100

    first

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle 
+"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """
+    Pascal's Triangle 
+    """
+    res = []
+    if n <= 0:
+        return res
+    for i in range(n):
+        tmp = []
+        for j in range(i+1):
+            tmp.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        res.append(tmp)
+
+    return res
